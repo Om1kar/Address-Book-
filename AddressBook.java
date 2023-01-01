36d1ff222ffaba015501654e3a46ef690940b895
omkar <omkardudhewar121@gmail.com>
2023-01-01 23:31:15 +0530

    added: sorting contacts

--- AddressBook.java
+++ AddressBook.java
@@ -125,6 +125,9 @@ public class AddressBook {
         }
         return false;
     }
+    public void printSorted() {
+        contactList.stream().sorted((contact1,contact2) -> contact1.firstName.compareTo(contact2.lastName)).forEach(contact -> System.out.println(contact));
+    }
 }
 
 
